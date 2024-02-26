df65593ff1cff21c683526885e9bf96d53e4546c
nickBes <58567842+nickBes@users.noreply.github.com>
2024-02-26 13:21:18 +0200

    added trino sql syntax highlighting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Editor } from "@monaco-editor/react";
 import "./App.css";
+import { Editor } from "@monaco-editor/react";
+import { language } from "monaco-sql-languages/out/esm/trinosql/trinosql";
+
+const TRINO_SQL_LANGUAGE_ID = "trinosql";
 
 function App() {
   return (
     <div id="editor-wrapper">
-      <Editor defaultLanguage="trinosql" theme="vs-dark" />
+      <Editor
+        defaultLanguage={TRINO_SQL_LANGUAGE_ID}
+        theme="vs-dark"
+        onMount={(editor, monaco) => {
+          editor.updateOptions({ minimap: { enabled: false } });
+          monaco.languages.register({
+            id: TRINO_SQL_LANGUAGE_ID,
+          });
+          monaco.languages.setMonarchTokensProvider(
+            TRINO_SQL_LANGUAGE_ID,
+            language
+          );
+        }}
+      />
     </div>
   );
 }
